99cb351c70cb9cbb7e43632730bdcd9f280be207
Swann Croiset <swann.croiset@bull.net>
2013-08-12 14:54:30 +0200

    faster checks : no host resolution with netstat

--- scripts/ceilometer/check_ceilometer-agent-compute.sh
+++ scripts/ceilometer/check_ceilometer-agent-compute.sh
@@ -31,7 +31,8 @@ STATE_DEPENDENT=4
 
 PID=$(pidof -x ceilometer-agent-compute)
 
-if ! KEY=$(netstat -epta 2>/dev/null | grep $PID 2>/dev/null | grep amqp) || test -z $PID
+AMQP_PORT=$(grep amqp /etc/services|head -n 1|cut -f 1 -d '/'| awk '{print $2}')
+if ! KEY=$(netstat -nepta 2>/dev/null | grep $PID 2>/dev/null | grep ":$AMQP_PORT") || test -z "$PID"
 then
     echo "Ceilometer Compute Agent is not connected to AMQP."
     exit $STATE_CRITICAL
