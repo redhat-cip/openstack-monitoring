179c886f5dbd17a629d26847263311c46be82522
gtt116 <gtt116@gmail.com>
2014-03-05 11:48:29 +0800

    Add nvs check script for keystone

---
+++ scripts/keystone/nvs_check_keystone-api.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Keystone API monitoring script
+#
+# Copyright © 2013 eNovance <licensing@enovance.com>
+#
+# Author: Emilien Macchi <emilien.macchi@enovance.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Requirement: curl
+#
+set -e
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+STATE_DEPENDENT=4
+
+usage ()
+{
+    echo "Usage: $0 [OPTIONS]"
+    echo " -h               Get help"
+    echo " -H <Auth URL>    URL for obtaining an auth token. Ex : http://localhost:5000/v2.0"
+    echo " -U <username>    Username to use to get an auth token"
+    echo " -P <password>    Password to use ro get an auth token"
+    echo " -T <timeout>     Timeout to get a token. Default: 10s"
+}
+
+while getopts 'hH:U:P:T:' OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 0
+            ;;
+        H)
+            export OS_AUTH_URL=$OPTARG
+            ;;
+        U)
+            export OS_USERNAME=$OPTARG
+            ;;
+        P)
+            export OS_PASSWORD=$OPTARG
+            ;;
+        T)
+            export TIMEOUT=$OPTARG
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+# Set default values
+OS_AUTH_URL=${OS_AUTH_URL:-"http://localhost:5000/v2.0"}
+
+if ! which curl >/dev/null 2>&1
+then
+    echo "curl is not installed."
+    exit $STATE_UNKNOWN
+fi
+
+START=`date +%s`
+TOKEN=$(curl -X 'POST' ${OS_AUTH_URL}/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'"}}}' -H 'Content-type: application/json' 2>&1 | grep token|awk '{print $6}'|grep -o '".*"' | sed -n 's/.*"\([^"]*\)".*/\1/p')
+END=`date +%s`
+
+TIME=$((END-START))
+TIMEOUT=${TIMEOUT:-10}
+
+if [ -z "$TOKEN" ]; then
+    TIME=-1
+fi
+
+#echo key value operator limit alert
+echo "TIME $TIME > $TIMEOUT Get token using {value}s, timeout {limit}s"
