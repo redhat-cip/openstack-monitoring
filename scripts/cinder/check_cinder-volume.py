68d5bf858ba141cd4e2b6dac18b74ad664408883
joel <joel.r.cooklin@intel.com>
2014-08-03 07:44:46 -0700

    Added optional insecure option to check_cinder-volume and check_nova-instance. Added optional network_id option to check_nova-instance.

--- scripts/cinder/check_cinder-volume.py
+++ scripts/cinder/check_cinder-volume.py
@@ -23,15 +23,15 @@
 #
 # Requirments: python-cinderclient, python-argparse, python
 # ## Arguments
-# 
+#
 # ### Required arguments
 # * `--auth_url`: Keystone URL
 # * `--username`: Username to use for authentication
 # * `--password`: Password to use for authentication
 # * `--tenant`: Tenant name to use for authentication
-# 
+#
 # ### Optional arguments
-# 
+#
 # * `-h`: Show the help message and exit
 # * `--region_name`: Region to select for authentication
 # * `--endpoint_url`: Override the catalog endpoint
@@ -43,35 +43,36 @@
 # * `--force_delete`: If matching volumes are found, delete them and add a notification in the message instead of getting out in critical state.
 # * `--api_version`: Version of the API to use. 1 by default.
 # * `--timeout`: Max number of second to create/destroy a volume (120 by default).
+# * `--insecure`: The server's certificate will not be verified.
 # * `--verbose`: Print requests on stderr
-# 
+#
 # ## Usage
-# 
+#
 # Create a test volume and delete it, only if no volume match
 # `volume_name` (here `monitoring_test` by default).  If there is any
 # volume it assumes that it's some leftover and exit in CRITICAL state,
 # notifying the rogue volumes.  Good for testing that everything works
 # properly in your environment.
-# 
+#
 # * `check_cinder-volume.py --auth_url $OS_AUTH_URL --username $OS_USERNAME --tenant $OS_TENANT_NAME --password $OS_PASSWORD`
-# 
+#
 # Here we mangle the endpoint url to override the one returned by the
 # catalog.  If we assume that the url returned by the catalog is behind
 # a load balancer, this enable the user to easely choose which API
 # server it's going to query.
-# 
+#
 # * `check_cinder-volume.py --auth_url $OS_AUTH_URL --username $OS_USERNAME --tenant $OS_TENANT_NAME --password $OS_PASSWORD --endpoint_url http://localhost`
-# 
+#
 # Here we force de deletion of any volume found with the matching name.
 # The number of found volume is returned in the output of the plugin.
-# 
+#
 # * `check_cinder-volume.py --auth_url $OS_AUTH_URL --username $OS_USERNAME --tenant $OS_TENANT_NAME --password $OS_PASSWORD --force_delete`
-# 
+#
 # For a asynchronous usage relative to a nagios check, one can use
 # [cache_check.py](https://github.com/gaelL/nagios-cache-check)
-# 
+#
 # * `cache_check.py -c "check_cinder-volume.py --auth_url $OS_AUTH_URL --username $OS_USERNAME --tenant $OS_TENANT_NAME --password $OS_PASSWORD"`
-# 
+#
 import os
 import sys
 import argparse
@@ -302,6 +303,9 @@ parser.add_argument('--availability_zone', metavar='availability_zone',
                     default=None,
                     help='Specify availability zone.')
 
+parser.add_argument('--insecure', action='store_true',
+                    help="The server's cert will not be verified")
+
 parser.add_argument('--verbose', action='count',
                     help='Print requests on stderr.')
 
@@ -316,7 +320,8 @@ try:
                          api_key=args.password,
                          auth_url=args.auth_url,
                          endpoint_type=args.endpoint_type,
-                         http_log_debug=args.verbose)
+                         http_log_debug=args.verbose,
+                         insecure=args.insecure)
 except Exception as e:
     script_critical("Error creating cinder communication object: %s" % e)
 
